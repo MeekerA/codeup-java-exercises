5fca9894333274c65282092fb4c4d1fadc39c051
Austin Meeker <austinmeeker@me.com>
2016-11-11 11:41:46 -0600

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Austin on 11/11/16.
+ */
+public class HelloWorld {
+    // psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello World!!");
+        System.out.println("Hello Mammoth!!");
+        System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
+    }
+}
