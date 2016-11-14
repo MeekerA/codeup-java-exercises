797ebe238f8a112b4f08ca130b39b511ee264e0d
Austin Meeker <austinmeeker@me.com>
2016-11-14 11:50:03 -0600

    Control Statements and Loops exercise

---
+++ src/ControlStatementsandLoops.java
@@ -0,0 +1,60 @@
+/**
+ * Created by Austin on 11/14/16.
+ */
+
+import java.util.Scanner;
+
+public class ControlStatementsandLoops {
+
+    public static void main(String[] args) {
+
+//        int i = 5;
+//
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int ii = 2;
+//
+//        do {
+//
+//            System.out.println(ii);
+//            ii += 2;
+//        } while (ii <= 100);
+
+//        int iii = 100;
+//
+//        do {
+//
+//            System.out.println(iii);
+//            iii -= 5;
+//
+//        } while (iii >= -10);
+
+//        long i = 2L;
+//
+//        do {
+//
+//            System.out.println(i);
+//            i *= i;
+//
+//        } while ( i < 1000000);
+
+
+//     -------------- Table of Powers --------
+
+//        int StopNumber;
+//        System.out.print("Enter a stop Number: ");
+//        Scanner sc = new Scanner(System.in);
+//
+//        StopNumber = sc.nextInt();
+//
+//        for (int i = 1; i < StopNumber; i += 1){
+//            System.out.println()
+//        }
+
+
+    }
+
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,46 @@
 /**
  * Created by Austin on 11/11/16.
  */
+
+import java.util.Scanner;
+
 public class HelloWorld {
-    // psvm
+
     public static void main(String[] args) {
-        //sout
-        System.out.println("Hello World!!");
-        System.out.println("Hello Mammoth!!");
-        System.out.println("Hello World!!");
-        System.out.println("Hello World!!");
-        System.out.println("Hello World!!");
+
+        int width;
+        int length;
+        String userInput = "no";
+
+        System.out.println("Welcome to the room Calculator.");
+
+        do {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter width: ");
+            width = sc.nextInt();
+            System.out.print("Enter the Length: ");
+            length = sc.nextInt();
+
+            System.out.println("The room has a parameter of " + perimeter(width, length));
+            System.out.println("The area is " + area(width, length));
+
+            System.out.print("Do you want to calculate another room? ");
+            userInput = sc.next();
+
+        } while (userInput.equals("yes"));
+
+        System.out.println("Thank you for using the room Calculator.");
     }
+
+    public static int perimeter(int width, int height) {
+     return 2 * width + 2 * height;
+    }
+
+    public static int area (int width, int height) {
+     return width * height;
+    }
+
 }
+
