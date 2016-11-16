1dc36ede667a5a1ce1c14065d4d5b341fd21df8b
Austin Meeker <austinmeeker@me.com>
2016-11-16 17:07:24 -0600

    Class Exercises

--- src/Methods.java
+++ src/Methods.java
@@ -8,47 +8,55 @@ public class Methods {
 
     // ----------- Methods exercise #3 --------------
 
-
+    public static void main(String[] args) {
+        String changeMe = "hello codeup!";
+        changeString(changeMe); // won't change the string
+        System.out.println(changeMe);
+    }
+    public static void changeString(String s) {
+        s = "Wub a lub a dub dub";
+        System.out.println(s);
+    }
 
 
     // ------------ Methods exercise #2 --------------
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//        int userinput;
+//        long factorialStart;
+//        String runFactorialCal;
+//
+//        System.out.println("Welcome to the Factorial Calculator!");
+//        do {
+//            Scanner sc = new Scanner(System.in);
+//            System.out.print("Enter an integer form 1 to 20: ");
+//            userinput = sc.nextInt();
+//
+//            if (userinput <= 0 || userinput > 20) {
+//                System.out.println("Please enter a number from 1 to 20.");
+//
+//            } else {
+//                System.out.print(userinput + "! = ");
+//                factorialStart = 1L;
+//
+//                for (int i = 1; i <= userinput; i++) {
+//                    factorialStart = factorialStart * i;
+//                    System.out.print(i);
+//
+//                    if (i != userinput) {
+//                        System.out.print(" x ");
+//                    }
+//                }
+//                System.out.println(" = " + factorialStart);
+//            }
+//            System.out.print("Do you want to calculate another Factorial? (y/n): ");
+//            runFactorialCal = sc.next();
+//
+//        } while (runFactorialCal.equalsIgnoreCase("y"));
+//        System.out.println("Thank you for using the Factorial Calculator!");
 
-        int userinput;
-        long factorialStart;
-        String runFactorialCal;
-
-        System.out.println("Welcome to the Factorial Calculator!");
-        do {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter an integer form 1 to 20: ");
-            userinput = sc.nextInt();
-
-            if (userinput <= 0 || userinput > 20) {
-                System.out.println("Please enter a number from 1 to 20.");
-
-            } else {
-                System.out.print(userinput + "! = ");
-                factorialStart = 1L;
-
-                for (int i = 1; i <= userinput; i++) {
-                    factorialStart = factorialStart * i;
-                    System.out.print(i);
-
-                    if (i != userinput) {
-                        System.out.print(" x ");
-                    }
-                }
-                System.out.println(" = " + factorialStart);
-            }
-            System.out.print("Do you want to calculate another Factorial? (y/n): ");
-            runFactorialCal = sc.next();
-
-        } while (runFactorialCal.equalsIgnoreCase("y"));
-        System.out.println("Thank you for using the Factorial Calculator!");
-
-    }
+//    }
 
     // --------------- Declared Methods for exercise #1 -----------------
 
---
+++ src/exceptions_and_error_handling.java
@@ -0,0 +1,78 @@
+/**
+ * Created by Austin on 11/15/16.
+ */
+
+import java.util.Scanner;
+
+public class exceptions_and_error_handling {
+
+    public static void main(String[] args) {
+
+        // ---------------- Exercise #3 Game Development 101 -----------------
+
+        // randomNum = getRandomNumber()
+        // userGuess = getUserGuess()
+        // validateUserInput()
+        // compareAndShowOutput(userGuess, randomNumber)
+
+//        int randomNum = getRandomNumber();
+//        int userGuess = getUserGuess();
+//
+//        do {
+//
+//            if (randomNum == userGuess) {
+//                System.out.println("Good Guess! The random number was " + randomNum);
+//            } else if (randomNum > userGuess) {
+//                System.out.println("HIGHER");
+//            } else {
+//                System.out.println("LOWER");
+//            }
+//        } while (randomNum != userGuess);
+
+
+        // ---------------- Exercise #1 Write a program that will recognize invalid
+        // inputs when the user requests information about students in a class. --------------
+
+//        String studentName;
+//        int age;
+//        String reason;
+//
+//
+//        Scanner input = new Scanner(System.in).useDelimiter("\n");
+//
+//        System.out.print("Whats your name? : ");
+//        studentName = input.next();
+//
+//        System.out.print("What is your age? : ");
+//        age = input.nextInt();
+//
+//        System.out.print("Why do you want to learn programing? : ");
+//        reason = input.next();
+//
+//        input.close();
+//
+//        System.out.println("Student's name is " + studentName);
+//        System.out.println("Student's age is " + age);
+//        System.out.println("The reason the student wanted to learn coding " + reason);
+
+//    }
+//
+//    public static int getRandomNumber() {
+//        return (int) Math.ceil(Math.random() * 100);
+//    }
+//
+//    public static int getUserGuess() {
+//        int userInput;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Please guess a number between 1 & 100: ");
+//
+//        try {
+//
+//        } catch () {
+//
+//        }
+//
+//        return userInput;
+    }
+
+}
