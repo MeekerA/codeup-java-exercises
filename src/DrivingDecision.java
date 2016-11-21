379afdce5a5ca429445774189a2f15ea049a69bb
Austin Meeker <austinmeeker@me.com>
2016-11-21 09:44:03 -0600

    Enumerations Exercises

---
+++ src/DrivingDecision.java
@@ -0,0 +1,31 @@
+
+/**
+ * Created by Austin on 11/21/16.
+ */
+
+public class DrivingDecision {
+
+    public enum TrafficLightColors {
+        RED, YELLOW, GREEN
+    }
+
+    public static void main(String[] args) {
+        TrafficLightColors lightColors = TrafficLightColors.GREEN;
+        makeDrivingDecision(lightColors);
+
+    }
+
+    public static void makeDrivingDecision(TrafficLightColors lightColor) {
+        switch(lightColor) {
+            case RED:
+                System.out.println("Red means STOP!");
+                break;
+            case YELLOW:
+                System.out.println("Yellow, Step on the Gas!");
+                break;
+            case GREEN:
+                System.out.println("Green says keep going!");
+        }
+    }
+
+}
