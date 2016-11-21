cbd397846409b26ad818fe133a74288ef754cb72
Austin Meeker <austinmeeker@me.com>
2016-11-21 16:53:00 -0600

    Started Batting Average

---
+++ src/BattingAverage.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Austin on 11/21/16.
+ */
+
+import java.util.Scanner;
+
+public class BattingAverage {
+
+    public static void main(String[] args) {
+        int battingNumber;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enters the player's number of at-bats? :");
+        battingNumber = sc.nextInt();
+        int[] battingRecord = new int[battingNumber];
+
+        for(int atBat : battingRecord) {
+            System.out.print()
+        }
+
+
+
+    }
+
+}
