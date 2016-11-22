ec3e41df7c25c72594c45be86b9cd6bbef1078d1
Austin Meeker <austinmeeker@me.com>
2016-11-22 09:30:55 -0600

    Batting Average working.
    Still need to add error handling.

--- src/BattingAverage.java
+++ src/BattingAverage.java
@@ -2,23 +2,45 @@
  * Created by Austin on 11/21/16.
  */
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BattingAverage {
 
     public static void main(String[] args) {
         int battingNumber;
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enters the player's number of at-bats? :");
-        battingNumber = sc.nextInt();
-        int[] battingRecord = new int[battingNumber];
-
-        for(int atBat : battingRecord) {
-            System.out.print()
-        }
+        double sum;
+        double battingAverage;
+        String userInput;
 
 
+        System.out.println("Welcome to the Batting Average Calculator.");
+
+        do {
+            sum = 0;
+            battingAverage = 0;
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the player's number of at-bats? ");
+            battingNumber = sc.nextInt();
+            int[] battingRecord = new int[battingNumber];
+
+            for (int i = 1; i < battingRecord.length + 1; i++) {
+                System.out.print("How many bases did the batter get at-bat #" + i + " ");
+                battingRecord[i - 1] = sc.nextInt();
+                sum += battingRecord[i - 1];
+            }
+            battingAverage = sum / battingRecord.length;
+//        System.out.println(Arrays.toString(battingRecord));
+            System.out.println("The batting average is " + battingAverage);
+
+            System.out.print("Do you want to calculate to Batting average of another player? (y/n) ");
+            userInput = sc.next();
+            sc.close();
+
+        } while (userInput.equalsIgnoreCase("y"));
+
+        System.out.println("Thank you for using the Batting Average Calculator.");
 
     }
 
